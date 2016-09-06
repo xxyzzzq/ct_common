e16eb873f3460c7ea680f5ae8417e08bebd8a4e8
Zhiqiang Zhang <xxyzzzq@gmail.com>
2016-09-06 23:26:32 +0100

    fix cpplint

--- tools/cpplint_all.sh
+++ tools/cpplint_all.sh
@@ -3,4 +3,4 @@
 cur_dir=`dirname $0`
 files=`find $cur_dir/../src -name *.h -and -not -name optional.h -or -name *.cpp`
 
-python third_party/cpplint/cpplint.py $files 2>&1 | grep -v "Done processing"
+python $cur_dir/../third_party/cpplint/cpplint.py $files 2>&1 | grep -v "Done processing"
