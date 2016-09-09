b8c5e51e8dbcc94e7ccec173e0cc6e0922a10960
Zhiqiang Zhang <xxyzzzq@gmail.com>
2016-09-09 22:57:59 +0100

    better doc script

--- tools/build_doc.py
+++ tools/build_doc.py
@@ -20,8 +20,6 @@ def wrap_content(title, body):
 parser = argparse.ArgumentParser()
 parser.add_argument('--out-dir', nargs=1, dest='OUTPUT_DIR', required=True,
                     help='The output directory name')
-parser.add_argument('--title', nargs=1, dest='TITLE', required=True,
-                    help='The title of the generated web page')
 parser.add_argument('FILE_NAME', nargs=1,
                     help='Other argument passed to markdown2')
 parsed_args = parser.parse_args()
@@ -30,9 +28,12 @@ input_name = parsed_args.FILE_NAME[0]
 input_name_no_ext = os.path.splitext(os.path.basename(input_name))[0]
 output_name = os.path.join(
     parsed_args.OUTPUT_DIR[0], input_name_no_ext + ".html")
+with open(input_name, 'r') as infile:
+    title = infile.readline()
+    title = title.strip('#').strip()
 with open(input_name, 'r') as infile, open(output_name, 'w') as outfile:
     body = infile.read()
-    outfile.write(wrap_content(parsed_args.TITLE[0], body))
+    outfile.write(wrap_content(title, body))
     success = True
 
 if not success:
