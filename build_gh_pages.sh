9e03994ae2c05695d9258b9253c37c6f57d06015
Zhiqiang Zhang <xxyzzzq@gmail.com>
2016-09-28 01:04:39 +0100

    adding modeling language doc

--- build_gh_pages.sh
+++ build_gh_pages.sh
@@ -5,5 +5,5 @@ mkdir -p gh_out out
 cd gh_out
 cmake ..
 make ct_common_docs
-cp -r bin/programming_guide.html bin/style ../out/
+cp -r bin/modeling_language.html bin/programming_guide.html bin/explicit_cast.png bin/style ../out/
 cd ..
